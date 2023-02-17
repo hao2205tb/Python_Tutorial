e36038a00dcaa6278d743640fc2a5dea045a3ae3
hao2205tb <hao2205tb@gmail.com>
2023-02-17 14:29:20 +0700

    update

--- Code/hello_world/highline.py
+++ Code/hello_world/highline.py
@@ -1,11 +1,23 @@
-myvar = 1234
-print(type(myvar))
+import re
 
-# <class 'int'>
+txt = "the abc123 in the bcd456 better than the abc123"
 
-mystr = "hello"
-print(type(mystr))
 
-# <class 'str'>
+x = re.findall("a[a-z]*", txt)
+print(x)
+#>>['abc', 'an', 'abc']
 
-print(symmetrical_num(n))
+x = re.search("a[a-z]*", txt)
+print(x)
+#>><re.Match object; span=(4, 7), match='abc'>
+print(x.start())
+#>>4
+
+x = re.findall("\d+", txt)
+print(x)
+#>>['123', '456', '123']
+
+x = re.findall("^the.*abc", txt)
+print(x)
+#>>['the abc123 in the bcd456 better than the abc']
+

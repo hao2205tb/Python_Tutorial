832f7255a157673edb0d0e27c00b7473913cb340
hao2205tb <hao2205tb@gmail.com>
2023-02-26 22:59:50 +0700

    update

--- Code/hello_world/highline.py
+++ Code/hello_world/highline.py
@@ -1,7 +1,9 @@
-# join list
-print(" and ".join(["123", "hello", "world"]))
-#>> Hello You @# 123
+list1 = [0, 2, 3, 1, 2, 4, 6, 4]
+list1.sort()
+print(list1)
+#>> [0, 1, 2, 2, 3, 4, 4, 6]
 
-# join list
-print(" and ".join(("123", "hello", "world")))
-#>> Hello You @# 123
+list1.sort(reverse=True)
+print(sorted(list1, reverse=True))
+#>> [6, 4, 4, 3, 2, 2, 1, 0]
+
